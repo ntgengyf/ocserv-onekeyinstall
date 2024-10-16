504db3924eabb334cfb2519ef1a2daf2180b255a
ntgengyf <53422151+ntgengyf@users.noreply.github.com>
2024-10-16 15:48:13 +0800

    Update c.sh

--- c.sh
+++ c.sh
@@ -75,7 +75,7 @@ Get_ip(){
     fi
 }
 Download_ocserv(){
-    wget --inet4-only "https://www.infradead.org/ocserv/download/ocserv-${ocserv_ver}.tar.xz" -O /usr/local/src/
+    wget --inet4-only "https://www.infradead.org/ocserv/download/ocserv-${ocserv_ver}.tar.xz" -O /usr/local/src/ocserv-${ocserv_ver}.tar.xz
     [[ ! -s "ocserv-${ocserv_ver}.tar.xz" ]] && echo -e "${Error} ocserv source download failed!" && rm -rf "ocserv/" && rm -rf "ocserv-${ocserv_ver}.tar.xz" && exit 1
     cd /usr/local/src/
     tar -xJf ocserv-${ocserv_ver}.tar.xz && cd ocserv-${ocserv_ver}
